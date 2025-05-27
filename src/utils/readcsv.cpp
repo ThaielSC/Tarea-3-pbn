8d526beaf1d6d288a1754482cc064defcab88a9b
ThaielSC <tmsantiago1@miuandes.cl>
2025-05-27 01:21:45 -0400

    Fix| Se arregla orientación al cargar mapa

--- src/utils/readcsv.cpp
+++ src/utils/readcsv.cpp
@@ -1,14 +1,13 @@
-#include "../classes/mazmorra.h"
+#include "..//utils/readcsv.h"
 #include "../classes/enemigo.h"
 #include "../classes/jefe.h"
-#include "..//utils/readcsv.h"
+#include "../classes/mazmorra.h"
 #include <fstream>
-#include <sstream>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
-
-//LECTURA MAZMORRAS
+// LECTURA MAZMORRAS
 vector<Mazmorra> leerMazmorras(const string &archivo) {
   ifstream file(archivo);
   string line;
@@ -28,9 +27,9 @@ vector<Mazmorra> leerMazmorras(const string &archivo) {
     // int totalSalaPrincipal = m.anchoPrincipal * m.altoPrincipal;
 
     // Leer sala principal
-    for (int i = 0; i < m.altoPrincipal; ++i) {
+    for (int i = 0; i < m.anchoPrincipal; ++i) {
       vector<string> fila;
-      for (int j = 0; j < m.anchoPrincipal; ++j) {
+      for (int j = 0; j < m.altoPrincipal; ++j) {
         getline(ss, valor, ',');
         fila.push_back(valor);
       }
@@ -59,64 +58,64 @@ vector<Mazmorra> leerMazmorras(const string &archivo) {
   return mazmorras;
 }
 
-//LECTURA ENEMIGOS
-vector<Enemigo> leerEnemigos(const string& filename) {
-    ifstream file(filename);
-    string line;
-    vector<Enemigo> enemigos;
+// LECTURA ENEMIGOS
+vector<Enemigo> leerEnemigos(const string &filename) {
+  ifstream file(filename);
+  string line;
+  vector<Enemigo> enemigos;
 
-    while (getline(file, line)) {
-        istringstream ss(line);
-        string token;
-        vector<string> datos;
-        while (getline(ss, token, ',')) {
-            datos.push_back(token);
-        }
-
-        int y = stoi(datos[0]);
-        int x = stoi(datos[1]);
-        int numMovs = stoi(datos[2]);
-        vector<pair<int, int>> movimientos;
-        for (int i = 0; i < numMovs; ++i) {
-            int dy = stoi(datos[3 + 2*i]);
-            int dx = stoi(datos[3 + 2*i + 1]);
-            movimientos.emplace_back(dy, dx);
-        }
-        int vida = stoi(datos[3 + 2*numMovs]);
-        int daño = stoi(datos[4 + 2*numMovs]);
-        int rango = stoi(datos[5 + 2*numMovs]);
-        int frecuencia = stoi(datos[6 + 2*numMovs]);
-
-        enemigos.emplace_back(y, x, vida, movimientos, daño, rango, frecuencia);
-    }
-
-    return enemigos;
-}
-
-Jefe leerJefe(ifstream& file) {
-    string line;
-    getline(file, line);
+  while (getline(file, line)) {
     istringstream ss(line);
     string token;
     vector<string> datos;
     while (getline(ss, token, ',')) {
-        datos.push_back(token);
+      datos.push_back(token);
     }
 
-    string nombre = datos[0];
-    int y = stoi(datos[1]);
-    int x = stoi(datos[2]);
-    int numMovs = stoi(datos[3]);
+    int y = stoi(datos[0]);
+    int x = stoi(datos[1]);
+    int numMovs = stoi(datos[2]);
     vector<pair<int, int>> movimientos;
     for (int i = 0; i < numMovs; ++i) {
-        int dy = stoi(datos[4 + 2*i]);
-        int dx = stoi(datos[4 + 2*i + 1]);
-        movimientos.emplace_back(dy, dx);
+      int dy = stoi(datos[3 + 2 * i]);
+      int dx = stoi(datos[3 + 2 * i + 1]);
+      movimientos.emplace_back(dy, dx);
     }
-    int vida = stoi(datos[4 + 2*numMovs]);
-    int daño = stoi(datos[5 + 2*numMovs]);
-    int rango = stoi(datos[6 + 2*numMovs]);
-    int frecuencia = stoi(datos[7 + 2*numMovs]);
+    int vida = stoi(datos[3 + 2 * numMovs]);
+    int daño = stoi(datos[4 + 2 * numMovs]);
+    int rango = stoi(datos[5 + 2 * numMovs]);
+    int frecuencia = stoi(datos[6 + 2 * numMovs]);
 
-    return Jefe(nombre, y, x, vida, movimientos, daño, rango, frecuencia);
+    enemigos.emplace_back(y, x, vida, movimientos, daño, rango, frecuencia);
+  }
+
+  return enemigos;
+}
+
+Jefe leerJefe(ifstream &file) {
+  string line;
+  getline(file, line);
+  istringstream ss(line);
+  string token;
+  vector<string> datos;
+  while (getline(ss, token, ',')) {
+    datos.push_back(token);
+  }
+
+  string nombre = datos[0];
+  int y = stoi(datos[1]);
+  int x = stoi(datos[2]);
+  int numMovs = stoi(datos[3]);
+  vector<pair<int, int>> movimientos;
+  for (int i = 0; i < numMovs; ++i) {
+    int dy = stoi(datos[4 + 2 * i]);
+    int dx = stoi(datos[4 + 2 * i + 1]);
+    movimientos.emplace_back(dy, dx);
+  }
+  int vida = stoi(datos[4 + 2 * numMovs]);
+  int daño = stoi(datos[5 + 2 * numMovs]);
+  int rango = stoi(datos[6 + 2 * numMovs]);
+  int frecuencia = stoi(datos[7 + 2 * numMovs]);
+
+  return Jefe(nombre, y, x, vida, movimientos, daño, rango, frecuencia);
 }
