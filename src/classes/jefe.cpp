5792fe2d0d253f62ed4836dfbb660b216c8e1f48
Melaooo <trmelin@miuandes.cl>
2025-05-30 17:43:00 -0400

    Commit antes del merge

--- src/classes/jefe.cpp
+++ src/classes/jefe.cpp
@@ -27,7 +27,7 @@ Jefe Jefe::leerDesdeStream(std::istream &is) {
   int x = stoi(datos[2]);
   int numMovs = stoi(datos[3]);
 
-  vector<pair<int, int>> movimientos;
+  vector<pair<int, int> > movimientos;
   for (int i = 0; i < numMovs; ++i) {
     int dy = stoi(datos[4 + 2 * i]);
     int dx = stoi(datos[4 + 2 * i + 1]);
